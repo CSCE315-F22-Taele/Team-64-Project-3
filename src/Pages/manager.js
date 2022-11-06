6c24570d0ee389676eb5d6504fd8fb7c9b57a646
Nick Knighton <nickk@tamu.edu>
2022-11-05 21:47:12 -0500

    added containers for the manager layout.  Used a grid effect so that the items line up correctly on any size screen

--- src/Pages/manager.js
+++ src/Pages/manager.js
@@ -3,7 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Card } from 'react-bootstrap';
 const picture = new URL("../Resources/KyleField.jpg", import.meta.url)
 
-const myStyle = {
+//Style for the Kyle Field BG
+const backgroundStyle = {
   height: '100vh',
   objectFit: 'cover',
   overflow: 'hidden',
@@ -14,43 +15,69 @@ const myStyle = {
 }
 
 const glassPane = {
+  //Positioning
   position: 'relative',
   margin: '5vh auto',
   top: '0',
   right: '0',
   bottom: '0',
   left:'0',
-  backgroundColor: 'rgba(90, 90, 90, .8)',   
-  backdropFilter: 'blur(10px)',
   height: '90vh',
   width: '90vw',
   overflow: 'hidden', 
+
+  //Glass Effect
+  backgroundColor: 'rgba(90, 90, 90, .8)',   
+  backdropFilter: 'blur(10px)',
+  
+  //Grid layout
+  display: 'grid',
+  gridTemplateColumns: 'repeat(2, 1fr)',
+  gap: '2vw',
+  // gridAutoRows: 'minmax(500px, auto)',
+  padding: '2%',
+}
+
+//Containers
+const menuItemsStyle = {
+  width: '100%',
+  height: '100%',
+}
+const reportStyle = {
+  width: '100%',
+  height: '100%',
+}
+const inventoryItemsStyle = {
+  width: '100%',
+  height: '100%',
+  gridColumn: '1/3',
 }
 
 const Manager = () => {
     return (
     <div>
-      <img src={picture} style={myStyle} alt='Kyle Field'/>
+      <img src={picture} style={backgroundStyle} alt='Kyle Field'/>
         <div style={glassPane}>
-          <Card style={{color: '#000'}}>
+          
+          <Card style={menuItemsStyle}>
             <Card.Body>
-              <Card.Title>
+              <Card.Title style={{textAlign:'center'}}>
                 Menu Items
               </Card.Title>
             </Card.Body>
           </Card>
           
-          <Card style={{color: '#000'}}>
+          <Card style={reportStyle}>
             <Card.Body>
-              <Card.Title>
+              <Card.Title style={{textAlign:'center'}}>
                 Reports
               </Card.Title>
             </Card.Body>
           </Card>
           
-          <Card style={{color: '#000'}}>
+          <Card style={inventoryItemsStyle}>
             <Card.Body>
-              <Card.Title>
+              <Card.Title style={{textAlign:'center'}}>
                 Inventory Items
               </Card.Title>
             </Card.Body>
