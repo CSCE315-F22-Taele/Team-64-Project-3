f51dc7ed9a32625a3183533af4a27472473d502a
Will-K-T <21willdog42@gmail.com>
2022-11-08 12:08:56 -0600

    added the ability to perform PUT, POST, and DELETE requests on the inventory table.

--- ManagerApp/views.py
+++ ManagerApp/views.py
@@ -24,6 +24,18 @@ def inventoryApi(request, id=0):
             inv_serializer.save()
             return JsonResponse("Added Successfully!", safe=False)
         return JsonResponse("Failed to add.", safe=False)
+    elif request.method == 'PUT':
+        inv_data = JSONParser().parse(request)
+        inv = Inventory.objects.get(item_id=inv_data['item_id'])
+        inv_serializer = inventorySerializer(inv, data=inv_data)
+        if inv_serializer.is_valid():
+            inv_serializer.save()
+            return JsonResponse("Updated Successfully!", safe=False)
+        return JsonResponse("Failed to update.", safe=False)
+    elif request.method == 'DELETE':
+        inv = Inventory.objects.get(item_id=id)
+        inv.delete()
+        return JsonResponse("Delete Successfull!", safe=False)
 
 
 @csrf_exempt
