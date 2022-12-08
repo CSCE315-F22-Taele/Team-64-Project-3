7751a8ea51bfb2538f56db3be28bad8b60e41a64
zacz <zacz@tamu.edu>
2022-12-08 14:38:32 -0600

    adjusted server clear as well

--- src/Pages/server.js
+++ src/Pages/server.js
@@ -212,7 +212,7 @@ const Server = () => {
               <Card.Body style={{ height: '1vh' }}>
                 <h2 style={{ textAlign: 'center', color: 'black' }}>Total: ${total.toFixed(2)}</h2>
                 <OrderDisplay order={order} menu={menuTable} />
-                <Button style={{marginLeft: '692px', marginTop: '-1300px', width: '70px', height: '60px', backgroundColor: 'rgba(80, 0, 0, .8)'}} onClick={(event) => {
+                <Button style={{marginLeft: '695px', marginTop: '-1270px', width: '70px', height: '60px', backgroundColor: 'rgba(80, 0, 0, .8)'}} onClick={(event) => {
                   setOrder([]); setTotal(0.00);}}>Clear Order</Button>
               </Card.Body>
             </Card>
